c7a88a88d569d91eefda8fb85423591d2936a354
MaguiSequeira <maguisequeira1@gmail.com>
2022-12-30 17:04:30 -0300

    subiendo los primeros archivos de mi proyecto

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log('hello!')
+
+
+
+let objetoJson = {}; //objeto vacio
+
+let objetoJsonn = new Object(); //nuevo objeto
+ objetoJsonn.nombre= "Magali"
+ objetoJsonn.apellido= "Sequeir"
+
+let objetJSON = {  //objeto con 2 atributos
+  nombre: "Magui",
+  apellido: "Sequeira"
+}
+
+let matriz = {  //matriz empleados 
+  "empleados":[
+    {
+      "nombre": "David",
+      "apellido": "Sequeira",
+      "dni": 45167234
+
+    },
+
+    {
+      "nombre": "Santiago",
+      "apellido": "Sequeira",
+      "dni": 45167234
+
+    },
+
+    {
+      "nombre": "Noemi",
+      "apellido": "Sequeira",
+      "dni": 45167234
+    }
+  ]
+}
+
+
+console.log(objetoJson)
+console.log(objetoJsonn)
+console.log(objetJSON)
+console.log(matriz)
